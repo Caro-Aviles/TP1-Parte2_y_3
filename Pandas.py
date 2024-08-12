490b568488953ed39b8c7c996b01488a453f9447
Caro-Aviles <c.avilesaviles@uandresbello.edu>
2024-08-12 19:55:21 -0400

    Estableciendo las PK en el Dataframe

--- Pandas.py
+++ Pandas.py
@@ -7,3 +7,10 @@ df_order_items = pd.read_csv('U:/CAVILES/CAPACITACION/2024/DATA_ENGINEER/TP1-Par
 df_order_payments = pd.read_csv('U:/CAVILES/CAPACITACION/2024/DATA_ENGINEER/TP1-Parte2_y_3/ecommerce_order_payments_dataset.csv')
 df_orders = pd.read_csv('U:/CAVILES/CAPACITACION/2024/DATA_ENGINEER/TP1-Parte2_y_3/ecommerce_orders_dataset.csv')
 df_products = pd.read_csv('U:/CAVILES/CAPACITACION/2024/DATA_ENGINEER/TP1-Parte2_y_3/ecommerce_products_dataset.csv')
+
+#ESTABLECIENDO LAS PK EN EL DATAFRAME
+df_customers.set_index('customer_id', inplace=True)
+df_order_items.set_index('order_id', inplace=True)
+df_order_payments.set_index('order_id', inplace=True)
+df_orders.set_index('order_id', inplace=True)
+df_products.set_index('product_id', inplace=True)
