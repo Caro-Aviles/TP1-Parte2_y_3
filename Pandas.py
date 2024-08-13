24eb1c016582eb17a1cd99c688e6eebbe23bf830
Caro-Aviles <c.avilesaviles@uandresbello.edu>
2024-08-12 20:13:59 -0400

    Obteniendo el nuemro total de clientes unicos y Calculando el promedio de valor  de pago por poedido

--- Pandas.py
+++ Pandas.py
@@ -14,3 +14,16 @@ df_order_items.set_index('order_id', inplace=True)
 df_order_payments.set_index('order_id', inplace=True)
 df_orders.set_index('order_id', inplace=True)
 df_products.set_index('product_id', inplace=True)
+
+#OBTENIENDO EL NUMERO TOTAL DE CLIENTES UNICOS
+num_clientes_unicos =  len(df_customers.index)
+#IMPRIMIENDO EL NUMERO TOTAL DE CLIENTES UNICOS
+print ("El numero total de clientes unicos es: ", num_clientes_unicos)
+
+#CALCULANDO EL PROMEDIO DE VALOR DE PAGO POR PEDIDO
+#LLENADO LOS VALORES DESCONOCIDOS CON 0
+df_order_payments['payment_value'] = df_order_payments['payment_value'].fillna(0)
+#CALCULANDO EL PROMEDIO
+prom_pago_pedido = df_order_payments['payment_value'].mean()
+#IMPRIMIENDO EL PROMEDIO
+print ("El promedio del valor de pago por pedido es: ", prom_pago_pedido)
